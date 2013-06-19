c6d48741e64cda5b287680dcd1187a3811ac6cc9
Aaron Florey <azza@jcaks.net>
2013-06-19 22:59:40 +1000

    gtfo random e

--- NMA/NMA.py
+++ NMA/NMA.py
@@ -20,7 +20,7 @@
 #along with this program.  If not, see http://www.gnu.org/licenses/.
 
 from xdm.plugins import *
-from libs.pyname import pynma
+from libs.pynam import pynma
 
 class NMA(Notifier):
     version = "0.1"
